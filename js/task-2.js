736de19fb0a5d58f0e8338f76320b253a2310dac
Dmytro Maniuta <ip722428@gmail.com>
2023-12-09 17:59:04 +0200

    finished task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,24 @@
 'use strict';
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.items.includes(itemToRemove)) {
+      this.items.splice(this.items.indexOf(itemToRemove), 1);
+      return this.items;
+    }
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
