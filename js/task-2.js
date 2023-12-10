fac108b085894a9d5ecd9af399012a48d7e9a364
Dmytro Maniuta <ip722428@gmail.com>
2023-12-10 17:41:29 +0200

    fixed mistakes

--- js/task-2.js
+++ js/task-2.js
@@ -10,16 +10,16 @@ class Storage {
   addItem(newItem) {
     this.#items.push(newItem);
   }
-  removeItem(itemToRemove) {
-    if (this.#items.includes(itemToRemove)) {
-      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
-    }
-  }
+  // removeItem(itemToRemove) {
+  //   if (this.#items.includes(itemToRemove)) {
+  //     this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+  //   }
+  // }
 }
 
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
