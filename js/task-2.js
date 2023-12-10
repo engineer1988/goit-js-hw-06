e61a5824899263fe8d305d790ae79740866341bd
Dmytro Maniuta <ip722428@gmail.com>
2023-12-10 17:09:04 +0200

    fixed mistakes

--- js/task-2.js
+++ js/task-2.js
@@ -1,17 +1,18 @@
 'use strict';
 class Storage {
+  #items;
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
   getItems() {
-    return this.items;
+    return this.#items;
   }
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    if (this.items.includes(itemToRemove)) {
-      this.items.splice(this.items.indexOf(itemToRemove), 1);
+    if (this.#items.includes(itemToRemove)) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
     }
   }
 }
